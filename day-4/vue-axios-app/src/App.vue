279beeb417330ce65dfec0917bbaa6de45a73adc
Reshma.Bhosale <reshmab@mkcl.org>
2021-05-13 17:19:57 +0530

    first commit with - use of vue-axios package

---
+++ day-4/vue-axios-app/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <PostsComponent></PostsComponent>
+  </div>
+</template>
+
+<script>
+import PostsComponent from './components/Posts-Component.vue'
+
+export default {
+  name: 'App',
+  components: {
+    PostsComponent
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ day-4/vue-axios-app/src/components/Posts-Component.vue
@@ -0,0 +1,25 @@
+<template>
+<div>Hello</div>
+</template>
+
+<script>
+export default {
+    name : "PostsComponent",
+    data() {
+        return {
+            posts: []
+        }
+    },
+    mounted() {
+        this.getData()
+    },
+    methods: {
+        getData() {
+            this.axios.get("http://localhost:3000/posts").then((res)=> {
+                this.posts = res.data
+                console.log(this.posts)
+            })
+        }
+    }
+}
+</script>
